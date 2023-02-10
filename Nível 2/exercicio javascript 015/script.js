c0f9863ee53bc36ec0b148c329adfd54e5f6e390
MarcosTenorio <marcosvtenoriodev@gmail.com>
2023-02-10 17:20:47 -0300

    exercicio javascript 015

---
+++ Nível 2/exercicio javascript 015/script.js
@@ -0,0 +1,19 @@
+let numberRandom = Number(Math.floor(101*(Math.random())))
+
+let result = document.getElementById('result')
+
+function start(){
+    let palpite = Number(prompt('Digite seu palpite'))
+    
+
+    if(palpite < numberRandom){
+        result.innerHTML += `<p> O número ${palpite} é <strong> MENOR</strong> que o número secreto</p>`
+    }else if (palpite > numberRandom){
+        result.innerHTML += `<p> O número ${palpite} é <strong> MAIOR </strong> que o número secreto</p>`
+    }else if (palpite == numberRandom){
+        result.innerHTML += `<p> O número ${palpite} é o número secreto, <strong> parabéns!!! </strong></p>`;
+        document.getElementById('button') .style.visibility = 'hidden'
+    }else {
+        result.innerHTML += `<p>Valor inválido</strong></p>`
+    }
+}
