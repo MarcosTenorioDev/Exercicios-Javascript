67ff86e411fe5411c9bb70c5b1f9d0118e6d3773
MarcosTenorio <marcosvtenoriodev@gmail.com>
2023-02-28 23:25:21 -0300

    exercicio javascript 021 - 50%

---
+++ Nível 3/exercício javascript 021/script.js
@@ -0,0 +1,37 @@
+ let arrayMusic = [];
+
+ function addDataToArray(){
+     let musicData = readData();
+     addData(musicData);
+
+     console.log(musicData)
+     console.log(arrayMusic)
+
+ }
+
+ function addData(musicData){
+     arrayMusic.push(musicData)
+
+ }
+
+
+ function readData(){
+    
+     let musicData = {};
+
+     musicData.singer = document.querySelector('input#singer').value;
+     musicData.musicName = document.querySelector('input#musicName').value;
+     musicData.musicDuration = document.querySelector('input#musicDuration').value;
+
+     return musicData;
+
+ }
+
+ function removeDataToObject(){
+
+
+
+ }
+
+
+
