817352e9bdddfb4da76f07cdd2d8b28af79acd37
MarcosTenorio <marcosvtenoriodev@gmail.com>
2023-03-06 19:23:28 -0300

    atividade javascript 022

---
+++ Nível 3/exercicio javascript 022/script.js
@@ -0,0 +1,25 @@
+/* 
+let numeros = [40,12,90,100,1,36]
+
+
+numeros.sort(function(a,b){
+  if(a > b) return 1
+  if(a < b) return -1
+})
+
+console.log(numeros)
+
+ */
+
+let numeros = [1,2,3,4,5,6,7,8,9,10]
+function numerosPares(numero){
+  let novoArray = [
+    ...numero
+  ] 
+  
+  let array = novoArray.filter((valor) => {
+    return novoArray.indexOf(valor) % 2 === 1 
+  })
+  return array
+}
+console.log(numerosPares(numeros))
