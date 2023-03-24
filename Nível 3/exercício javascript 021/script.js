4ba79a73a4d6b2772918e88100ba4b35e270df0f
Alex <alexjosedosantos@gmail.com>
2023-03-24 17:48:08 -0300

    exercicio javascript 021

--- Nível 3/exercício javascript 021/script.js
+++ Nível 3/exercício javascript 021/script.js
@@ -1,12 +1,54 @@
-let amostra = () => (12 , 23 , 79).split(',');
+let arrayMusic = [];
+class music{
+    id;
+    singer;
+    name;
+    duration;
 
-//TODO: Print no console, para cada índice da lista, os parâmetros
-//se a amostra está bom, aceitável ou inaceitável
-console.log(amostra)
-function checkHealth(){
-  if(amostra > 75) {
-  print(`Descartar da lista`);
-  }
+    constructor(singer, name, duration){
+      this.id;
+      this.singer;
+      this.name;
+      this.duration;
+    }
+
+
 }
+
+ function addDataToArray(){
+    let music = readData();
+     readData();
+     addData(music);
+  
+
+     console.log(music)
+     console.log(arrayMusic)
+     
+
+ }
+
+ function addData(music){
+     arrayMusic.push(music)
+
+ }
+
+
+ function readData(){
+
+  let music = {}
+
+  music.singer = document.querySelector('input#singer').value;
+  music.name = document.querySelector('input#name').value;
+  music.duration = document.querySelector('input#duration').value;
+
+  return music;
+
+  
+ }
+
+ function result(){
+   let
+ }
+
 
 
